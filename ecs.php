a998af1edafd318b5840cda5b3a843fb0fb8c2cb
Tomas <tomasjakll@gmail.com>
2025-06-13 08:38:59 +0200

    chore(static analysis): setup ecs, phpstan and phpunit

---
+++ ecs.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Phpdoc\PhpdocAlignFixer;
+use PhpCsFixer\Fixer\Whitespace\MethodChainingIndentationFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return ECSConfig::configure()
+    ->withParallel()
+    ->withPaths([
+        __DIR__ . '/src/Xutim/Bundle/CoreBundle/src',
+        __DIR__ . '/src/Xutim/Bundle/EventBundle/src',
+        __DIR__ . '/src/Xutim/Bundle/CoreBundle/tests',
+        __DIR__ . '/src/Xutim/Bundle/EventBundle/tests',
+    ])
+    ->withSets([
+        SetList::CLEAN_CODE,
+        SetList::PSR_12,
+        SetList::STRICT,
+    ])
+    ->withRules([
+        PhpdocAlignFixer::class,
+        MethodChainingIndentationFixer::class
+    ])
+    ->withSkip([
+        MethodChainingIndentationFixer::class => [
+            '*/DependencyInjection/Configuration.php',
+        ],
+    ])
+;
