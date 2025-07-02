4668663c46e0b0cbf47ea71036de26f4bff9ee4f
Tomas <tomasjakll@gmail.com>
2025-07-02 09:00:12 +0200

    feat(domain): add a Domain package for shared domain logic

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -6,6 +6,7 @@ use Symplify\MonorepoBuilder\Config\MBConfig;
 
 return static function (MBConfig $mbConfig): void {
     $mbConfig->packageDirectories([
+        __DIR__ . '/src/Xutim',
         __DIR__ . '/src/Xutim/Bundle',
         __DIR__ . '/src/Xutim/Component',
     ]);
