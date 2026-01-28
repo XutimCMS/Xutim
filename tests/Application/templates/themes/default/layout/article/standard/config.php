d289d8ff4ed12dbac24411a2beae6005f9249131
Tomas <tomasjakll@gmail.com>
2026-01-28 07:30:10 +0100

    test(core-bundle): add test theme templates for article and page previews

---
+++ tests/Application/templates/themes/default/layout/article/standard/config.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Xutim\CoreBundle\Config\Layout\LayoutConfig;
+
+return new LayoutConfig(
+    code: 'standard',
+    name: 'Standard Article',
+    default: true
+);
