70af0ad1f9d256c1ccb0fc7bb94d0ad9ccb839f6
Tomas <tomasjakll@gmail.com>
2026-03-25 13:37:51 +0100

    fix(core): add unique constraints for article/page locale

--- tests/Application/src/Entity/Core/ContentTranslation.php
+++ tests/Application/src/Entity/Core/ContentTranslation.php
@@ -6,10 +6,14 @@ namespace App\Entity\Core;
 
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping\UniqueConstraint;
 use Xutim\CoreBundle\Entity\ContentTranslation as BaseContentTranslation;
 
 #[Entity]
 #[Table(name: 'app_content_translation')]
+#[UniqueConstraint(columns: ['slug', 'locale'])]
+#[UniqueConstraint(columns: ['article_id', 'locale'])]
+#[UniqueConstraint(columns: ['page_id', 'locale'])]
 class ContentTranslation extends BaseContentTranslation
 {
 }
