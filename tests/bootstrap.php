4cb0f4d3761f61e18fd8ba11cab3b28243bd9100
Tomas <tomasjakll@gmail.com>
2026-03-31 12:57:51 +0200

    feat(core): rewrite diff engine with canonical content model

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,3 +3,7 @@
 declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
+
+if (is_file(__DIR__ . '/Application/vendor/autoload.php')) {
+    require_once __DIR__ . '/Application/vendor/autoload.php';
+}
