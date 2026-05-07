0de52dc370b9a7d5c15ed3ce3e207e8d8c30bce3
Tomas <tomasjakll@gmail.com>
2026-05-07 11:53:34 +0200

    fix(core): bound page move-down and stabilise tree ordering
    - movePosDown now requires max position; clamps instead of overshooting
    - movePosUp short-circuits when already at 0
    - hierarchyByPublished and findAllPaths gain createdAt/id tie-breakers
    - test app Page enforces (parent_id, position) uniqueness

--- tests/Application/src/Entity/Core/Page.php
+++ tests/Application/src/Entity/Core/Page.php
@@ -6,10 +6,12 @@ namespace App\Entity\Core;
 
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping\UniqueConstraint;
 use Xutim\CoreBundle\Entity\Page as BasePage;
 
 #[Entity]
 #[Table(name: 'app_page')]
+#[UniqueConstraint(name: 'uniq_page_parent_position', columns: ['parent_id', 'position'])]
 class Page extends BasePage
 {
 }
